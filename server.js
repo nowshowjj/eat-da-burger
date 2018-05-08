699be01eba020b0e68750db2d8a0cdbd52a5da94
J.J. Ayala <jjayala83@gmail.com>
2018-05-07 23:07:26 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var db = require("./models");
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+
+var port = process.env.PORT || 3000;
+db.sequelize.sync().then(function() {
+  app.listen(port);
+});
+
+console.log(module.exports);
